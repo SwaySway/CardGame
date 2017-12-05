97c4b15180de94570f78b105add64e947084c8b6
ieabbas <the0nlyismail@gmail.com>
2017-12-04 20:11:03 -0800

    Base of media player code, needs refinement

--- app/src/main/java/com/example/josue/cardgame/BGMService.java
+++ app/src/main/java/com/example/josue/cardgame/BGMService.java
@@ -1,8 +1,10 @@
 package com.example.josue.cardgame;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.media.MediaPlayer;
 
 /**
  * Created by josue on 12/4/2017.
@@ -10,14 +12,57 @@ import android.os.IBinder;
 
 public class BGMService extends Service {
 
-    public IBinder onBind(Intent intent){
+    private static final String TAG = null;
+    MediaPlayer player;
+
+    public IBinder onBind(Intent i){
         return null;
     }
 
-    public void onCreate(){
+//public void onCreate(){
+//super.onCreate();
+//
+//
+//}
+//
+    @Override
+    public void onCreate() {
         super.onCreate();
-
+        //player = MediaPlayer.create(this, R.raw.idil);
+        player.setLooping(true); // Set looping
+        player.setVolume(100,100);
 
     }
+    // This suppress comment is the only thing keeping the method from being uncompilable
+    // Remove when we can figure out what to return, Android Studio has some suggestions
+    @SuppressLint("WrongConstant")
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return 1;
+    }
 
+    public void onStart(Intent intent, int startId) {
+        // TO DO
+    }
+    public IBinder onUnBind(Intent arg0) {
+        // TO DO Auto-generated method
+        return null;
+    }
+
+    public void onStop() {
+
+    }
+    public void onPause() {
+
+    }
+    @Override
+    public void onDestroy() {
+        player.stop();
+        player.release();
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
 }
